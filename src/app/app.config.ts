b5c7caf84830e0532b0af217a9125e289e75e226
Anil Kumar <anilkumar.dcttechnology@gmail.com>
2025-06-23 13:32:43 +0530

    updated task component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+} from '@angular/router';
 import { routes } from './app.routes';
 
 export const config: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding())], //for getting the data on based id
+  providers: [
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always',
+      })
+    ),
+  ], //for getting the data on based id
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/new-task/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -14,8 +15,17 @@ export const routes: Routes = [
     path: 'users/:userId', //dynamic route-> your-domain/users/u1
     component: UserTasksComponent,
     children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'prefix',
+      },
       { path: 'tasks', component: TasksComponent },
       { path: 'tasks/new', component: NewTaskComponent },
     ], //use of nested routing
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
